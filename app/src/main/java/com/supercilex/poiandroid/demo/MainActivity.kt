cbeb12d60421482572055ce2e066c16abab852bc
Witalo Albuquerque <witalocarlos@gmail.com>
2020-05-24 20:37:07 -0400

    fixed XmlStreamReader notFound

--- app/src/main/java/com/supercilex/poiandroid/demo/MainActivity.kt
+++ app/src/main/java/com/supercilex/poiandroid/demo/MainActivity.kt
@@ -3,63 +3,39 @@ package com.supercilex.poiandroid.demo
 import android.Manifest
 import android.content.Intent
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
-import org.apache.poi.hssf.usermodel.HSSFSheet
-import org.apache.poi.hssf.usermodel.HSSFWorkbook
-import org.apache.poi.hwpf.HWPFDocument
-import org.apache.poi.hwpf.usermodel.Paragraph
-import org.apache.poi.hwpf.usermodel.ParagraphProperties
-import org.apache.poi.poifs.filesystem.POIFSFileSystem
 import org.apache.poi.ss.usermodel.HorizontalAlignment
 import org.apache.poi.ss.usermodel.VerticalAlignment
-import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.ss.util.CellRangeAddress
+import org.apache.poi.xddf.usermodel.chart.AxisCrosses
 import org.apache.poi.xddf.usermodel.chart.AxisPosition
 import org.apache.poi.xddf.usermodel.chart.ChartTypes
 import org.apache.poi.xddf.usermodel.chart.XDDFDataSourcesFactory
 import org.apache.poi.xssf.usermodel.XSSFSheet
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
-import org.apache.poi.xwpf.usermodel.XWPFDocument
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTTitle
 import pub.devrel.easypermissions.EasyPermissions
 import java.io.File
-import java.io.FileInputStream
 import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         init {
-//            System.setProperty(
-//                    "org.apache.poi.javax.xml.stream.XMLInputFactory",
-//                    "com.fasterxml.aalto.stax.InputFactoryImpl"
-//            )
-//            System.setProperty(
-//                    "org.apache.poi.javax.xml.stream.XMLOutputFactory",
-//                    "com.fasterxml.aalto.stax.OutputFactoryImpl"
-//            )
-//            System.setProperty(
-//                    "org.apache.poi.javax.xml.stream.XMLEventFactory",
-//                    "com.fasterxml.aalto.stax.EventFactoryImpl"
-//            )
-//            System.setProperty("org.apache.poi.javax.xml.stream.XMLStreamReader",
-//                    "com.fasterxml.aalto.stax.StreamReaderImpl")
+            System.setProperty("org.apache.poi.javax.xml.stream.XMLInputFactory", "com.fasterxml.aalto.stax.InputFactoryImpl")
+            System.setProperty("org.apache.poi.javax.xml.stream.XMLOutputFactory", "com.fasterxml.aalto.stax.OutputFactoryImpl")
+            System.setProperty("org.apache.poi.javax.xml.stream.XMLEventFactory", "com.fasterxml.aalto.stax.EventFactoryImpl")
+            System.setProperty("org.apache.poi.javax.xml.stream.XMLStreamReader", "com.fasterxml.aalto.stax.StreamReaderImpl")
+
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        System.setProperty("org.apache.poi.javax.xml.stream.XMLInputFactory", "com.fasterxml.aalto.stax.InputFactoryImpl");
-        System.setProperty("org.apache.poi.javax.xml.stream.XMLOutputFactory", "com.fasterxml.aalto.stax.OutputFactoryImpl");
-        System.setProperty("org.apache.poi.javax.xml.stream.XMLEventFactory", "com.fasterxml.aalto.stax.EventFactoryImpl");
-        System.setProperty("org.apache.poi.javax.xml.stream.XMLStreamReader", "com.fasterxml.aalto.stax.StreamReaderImpl");
-
 
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
@@ -101,59 +77,16 @@ class MainActivity : AppCompatActivity() {
             file.delete()
         }
 
-        val word = XWPFDocument()
-
-
-//        val workbook = HSSFWorkbook()
-//        workbook.createSheet("Test").populate()
-//        workbook.write(FileOutputStream(file))
-
-        try {
-
-            val input = resources.openRawResource(R.raw.poi_empty)
-            val fs = POIFSFileSystem(input)
-            val hwpfDocument = HWPFDocument(fs)
-
-
-            val text = "Word doc\nSample.\nAndroid POI\nHello World."
-            val paragraphs = text.split("\n")
-
-            hwpfDocument.apply {
-                var paragraph = range.getParagraph(range.numParagraphs()-1)
-                paragraphs.forEach { p ->
-                    run {
-                        paragraph.spacingAfter = 200
-                        paragraph.setJustification(1.toByte())
-                        val insertAfter = paragraph.insertAfter(p)
-                        //  insertAfter.replaceText(p, true);
-                        insertAfter.fontSize = 15
-                        insertAfter.color = R.color.colorAccent
-                        insertAfter.isBold = true
-                        paragraph = insertAfter.getParagraph(range.numParagraphs()-1)
-                    }
-                }
-
-                val output = FileOutputStream(file)
-                write(output)
-                output.close()
-                input.close()
-            }
-
-
-
-
-
-
-        } catch (e: Exception) {
-            Log.e("POI ANDROID", e.message)
-        }
+        val workbook = XSSFWorkbook()
+        workbook.createSheet("Test").populate()
+        workbook.write(FileOutputStream(file))
 
 
         val uri = FileProvider.getUriForFile(this, "$packageName.provider", file)
         return Intent()
                 .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 .setAction(Intent.ACTION_VIEW)
-                .setDataAndType(uri, "application/vnd.ms-word")
+                .setDataAndType(uri, "application/vnd.ms-excel")
                 .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
                 .also {
                     if (it.resolveActivity(packageManager) == null) {
@@ -165,51 +98,56 @@ class MainActivity : AppCompatActivity() {
     /**
      * Add random stuff to a spreadsheet to make sure everything works
      */
-    private fun HSSFSheet.populate() {
+    private fun XSSFSheet.populate() {
         createFreezePane(1, 1)
         createRow(0).apply {
             createCell(0).apply {
                 setCellValue(0.0)
-//                cellStyle = workbook.createCellStyle().apply {
-//                    dataFormat = workbook.createDataFormat().getFormat("0.00")
-//                    setFont(workbook.createFont().apply { bold = true })
-//                    setAlignment(HorizontalAlignment.CENTER)
-//                    setVerticalAlignment(VerticalAlignment.CENTER)
+                cellStyle = workbook.createCellStyle().apply {
+                    dataFormat = workbook.createDataFormat().getFormat("0.00")
+                    setFont(workbook.createFont().apply { bold = true })
+                    setAlignment(HorizontalAlignment.CENTER)
+                    setVerticalAlignment(VerticalAlignment.CENTER)
             }
         }
-//            createCell(1).apply {
-//                setCellValue("Wassup?")
-//            }
-//            createCell(2).apply {
-//                createRow(1).createCell(0).setCellValue(1.0)
-//                cellFormula = "SUM(${CellRangeAddress(0, 1, 0, 0).formatAsString()})"
-//            }
-        // }
+            createCell(1).apply {
+                setCellValue("Wassup?")
+            }
+            createCell(2).apply {
+                createRow(1).createCell(0).setCellValue(1.0)
+                cellFormula = "SUM(${CellRangeAddress(0, 1, 0, 0).formatAsString()})"
+            }
+         }
 
-//        createDrawingPatriarch().run {
-//            createChart(createAnchor(0, 0, 0, 0, 4, 2, 10, 6))
-//        }.apply {
-//
-//           plot(createData(ChartTypes.LINE, createCategoryAxis(AxisPosition.BOTTOM), createValueAxis(AxisPosition.LEFT)).apply {
-//                addSeries(
-//                        XDDFDataSourcesFactory.fromStringCellRange(
-//                                this@populate,
-//                                CellRangeAddress(0, 0, 1, 1)
-//                        ),
-//                        XDDFDataSourcesFactory.fromNumericCellRange(
-//                                this@populate,
-//                                CellRangeAddress(0, 1, 0, 0)
-//                        )
-//                ).setTitle("Foobar", setSheetTitle("", 0))
-//
-//            })
-//
-//
-//                val plotArea = ctChart.plotArea
-//                plotArea.getValAxArray(0).addNewTitle().setValue("Values")
-//                plotArea.getCatAxArray(0).addNewTitle().setValue("Title")
-//
-//        }
+        createDrawingPatriarch().run {
+            createChart(createAnchor(0, 0, 0, 0, 4, 2, 10, 6))
+        }.apply {
+
+            val leftAxis = createValueAxis(AxisPosition.LEFT)
+            leftAxis.crosses = AxisCrosses.AUTO_ZERO
+            val bottomAxis = createCategoryAxis(AxisPosition.BOTTOM)
+
+            val dataSourceFactory = XDDFDataSourcesFactory.fromNumericCellRange(
+                    this@populate,
+                    CellRangeAddress(0, 0, 0, 1)
+            )
+
+            val seriesOne = XDDFDataSourcesFactory.fromNumericCellRange(
+                    this@populate,
+                    CellRangeAddress(1, 1, 0, 1)
+            )
+
+
+            plot(createData(ChartTypes.LINE, bottomAxis, leftAxis).apply {
+                addSeries(dataSourceFactory, seriesOne).setTitle("Foobar", setSheetTitle("", 0))
+            })
+
+
+                val plotArea = ctChart.plotArea
+                plotArea.getValAxArray(0).addNewTitle().setValue("Values")
+                plotArea.getCatAxArray(0).addNewTitle().setValue("Title")
+
+        }
     }
 
     private fun CTTitle.setValue(text: String) {
